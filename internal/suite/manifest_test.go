3c12c00a64b4c2fcd000acbbcfb3b86d950a6ca1
Patrick Young <patrick.young@mac.com>
2026-08-22 16:47:46 -0400

    bench: version the synchronized suite as 0.6.0

--- internal/suite/manifest_test.go
+++ internal/suite/manifest_test.go
@@ -10,7 +10,7 @@ func TestEmbeddedManifestIsComplete(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if m.Version != "0.5.0" || len(m.Components) != 6 {
+	if m.Version != "0.6.0" || len(m.Components) != 6 {
 		t.Fatalf("manifest version=%q components=%d", m.Version, len(m.Components))
 	}
 	data, err := JSON(m)
